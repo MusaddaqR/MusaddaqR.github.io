7d91142e5d43af7fa0dfc6cf6fe43314608bfbe0
MusaddaqR <musaddaqr@hotmail.com>
2020-11-17 11:28:39 +0000

    update

--- products.js
+++ products.js
@@ -1,47 +1,94 @@
 let products = [
     {
       "id": 1001,
-      "title": "Cat Food, 25lb bag",
-      "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
-      "price": 20.00,
+      "subject": "Mathematics",
+      "location": "London",
+      "price": "£" + 20.00,
       "image": "images/product-fullsize.png",
-      "availableInventory": 10,
+      "availableInventory": 5,
       "rating": 2
     },
     {
       "id": 1002,
-      "title": "Yarn",
-      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
-      "price": 2.99,
-      "image": "images/yarn.jpg",
-      "availableInventory": 7,
-      "rating": 3
+      "subject": "English",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
     },
     {
       "id": 1003,
-      "title": "Kitty Litter",
-      "description": "Premium kitty litter for your cat.",
-      "price": 11.00,
-      "image": "images/cat-litter.jpg",
-      "availableInventory": 99,
-      "rating": 4
+      "subject": "Science",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
     },
     {
       "id": 1004,
-      "title": "Cat House",
-      "description": "A place for your cat to play!",
-      "price": 7.99,
-      "image": "images/cat-house.jpg",
-      "availableInventory": 11,
-      "rating": 5
+      "subject": "Geography",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
     },
     {
       "id": 1005,
-      "title": "Laser Pointer",
-      "description": "Drive your cat crazy with this <em>amazing</em> product.",
-      "price": 49.99,
-      "image": "images/laser-pointer.jpg",
-      "availableInventory": 25,
-      "rating": 4
-    }
+      "subject": "History",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1006,
+      "subject": "International Technology",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1007,
+      "subject": "Design Technology",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1008,
+      "subject": "Food Technology",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1009,
+      "subject": "Religious Studies",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1010,
+      "subject": "Drama",
+      "location": "London",
+      "price": "£" + 20.00,
+      "image": "images/product-fullsize.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    
+  
   ]
