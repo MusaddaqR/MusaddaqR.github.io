09bc11b1a948cdf29637b137861a6a79c0e8ea0e
MusaddaqR <musaddaqr@hotmail.com>
2020-11-20 14:10:47 +0000

    updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
-        let webstore = new Vue({
+        let lessonApp = new Vue({
             el: '#app',
-            data: {
+            data () {
+                return {
                 sitename: 'Web app with Vue.js',
                 products: products,
                 cart: [],
@@ -9,9 +10,52 @@
                 order: {
                     firstName: null,
                     phoneNumber: null,    
-                },       
-            },
+                }       
+            }
+        },
      
+
+
+
+        computed: {
+
+            nameIsValid () {
+                return !!this.order.Name
+            },
+
+            phoneIsValid () {
+                return typeof this.order.phoneNumber === 
+                'number' && this.order.phoneNumber > 01111111111
+
+            },
+
+            formIsValid() {
+                return this.nameIsValid && this.phoneIsValid
+
+            },
+
+            cartItemCount() {
+                return this.cart.length;
+            },  
+
+            sortedProducts() {
+                let productsArray = this.products.slice(0);
+                function compare(a, b) {
+                    if (a.price > b.price)
+                        return 1;
+                    if (a.price < b.price)
+                        return -1;
+                    return 0;
+                }
+                return productsArray.sort(compare);
+            }
+            
+        },
+
+
+
+
+
             methods: {
                 addToCart(product) {
                     this.cart.push(product.id);
@@ -20,9 +64,19 @@
                 showCheckout() {
                     this.showProduct = this.showProduct ? false : true;
                 },
+
+                
                 submitForm() {
-                    alert('The order has been succesful!')
-                },
+                    const formIsValid = this.nameIsValid && this.phoneIsValid
+                    if (formIsValid) {
+                    alert('The order has been succesful!',this.order)
+                }else {
+                    alert('invalid form')
+                }
+            },
+
+            
+
                 canAddToCart(product) {
                     return product.availableInventory > this.cartCount(product.id);
                 },
@@ -41,22 +95,5 @@
 
                 
             },
-            computed: {
-                cartItemCount() {
-                    return this.cart.length;
-                },  
-    
-                sortedProducts() {
-                    let productsArray = this.products.slice(0);
-                    function compare(a, b) {
-                        if (a.price > b.price)
-                            return 1;
-                        if (a.price < b.price)
-                            return -1;
-                        return 0;
-                    }
-                    return productsArray.sort(compare);
-                }
-                
-            },
+
         });
